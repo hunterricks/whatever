ee82fe334c1778584a2d2e7b96be7d184d3a960c
hunterricks <huntermatthewricks@gmail.com>
2024-10-22 21:12:02 -0500

    Fix Next.js route issues and update authentication

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("afef8f40c7096a87")
+/******/ 		__webpack_require__.h = () => ("a6861c91b105ce62")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8389f2e24de0144"; }
+/******/ 		__webpack_require__.h = function() { return "ef128a0c6280e11e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
