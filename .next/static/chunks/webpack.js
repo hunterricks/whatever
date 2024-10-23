5b046743755197469e3d609789ed4e2186c23faf
hunterricks <huntermatthewricks@gmail.com>
2024-10-22 21:02:20 -0500

    Fix build errors: remove example route and fix syntax in messages page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8397dc39ff059eb8"; }
+/******/ 		__webpack_require__.h = function() { return "893aba0c5503824a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
